1bfd8eec7110cd961e0d9155a9883d49482ed63c
Alix Warnke <alix.warnke@gmail.com>
2016-04-22 22:20:12 +0200

    Test cleanup

--- fatman-domain/src/test/java/com/sundbybergsit/calculation/BmiCalculatorTest.java
+++ fatman-domain/src/test/java/com/sundbybergsit/calculation/BmiCalculatorTest.java
@@ -13,15 +13,12 @@ class BmiCalculatorTest {
     public void bmiCalculated() throws Exception {
         FatmanDbUser dummyUser = mock(FatmanDbUser.class);
         when(dummyUser.getHeightInCentimetres()).thenReturn(170);
-        Calculator<FatmanDbUser> bmiCalculator = new Calculator<FatmanDbUser>() {
-            @Override
-            public Number calculate(FatmanDbUser user, Number weight) {
-                return (float) weight / (((double) user.getHeightInCentimetres() / 100) * ((double) user.getHeightInCentimetres() / 100));
-            }
-        };
-//        Calculator<FatmanDbUser> bmiCalculator =
-//                (user, weight) -> (float) weight /
-//                        (((double) user.getHeightInCentimetres() / 100) * ((double) user.getHeightInCentimetres() / 100));
+        Calculator<FatmanDbUser> bmiCalculator =
+                (user, weight) ->
+                        (float) weight / (
+                                ((double) user.getHeightInCentimetres() / 100) *
+                                        ((double) user.getHeightInCentimetres() / 100)
+                        );
         Number bmi = bmiCalculator.calculate(dummyUser, 65.0F);
         assertTrue(bmi.doubleValue() < 23 && bmi.doubleValue() > 22);
     }
